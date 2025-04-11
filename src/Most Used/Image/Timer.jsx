59c5628816d8ba6986c26daae92b68be3ddae496
Kishibe14 <27.08.07@mail.ru>
2025-04-11 17:27:03 +1100

    third commit

--- src/Most Used/Image/Timer.jsx
+++ src/Most Used/Image/Timer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Timerblack from '../../Most Used/Image/Timerblack.svg'; 
+import TIMERblack from '../../Most Used/Image/TIMERblack.svg'; 
 
 function Timer() {
 
   return(
     <div>
-      <img src={Timerblack} alt="Star" style={{ width: '20px', height: '20px' }}/>
+      <img src={TIMERblack} alt="Star" style={{ width: '20px', height: '20px' }}/>
     </div>
   );
 }
