44acf27975d428b13bfaf9554ddeb64e5ada4e1f
Kishibe14 <27.08.07@mail.ru>
2025-04-12 17:24:52 +1100

    six commit

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -124,6 +124,20 @@ function HomePage() {
       <div className='for-margin-home'></div>
       <span className='points-count'>{points.toFixed(4)}</span>
       <DayCheck onPointsUpdate={handlePointsUpdate} />
+      <div className='container-game'>
+        <div className='left-section-gif-game'>
+          <GrHeart />
+        </div>
+        <div className='mid-section-textabout-game'>
+          <span className='first-span-game'>Mini Game</span> 
+          <span className='second-span-game'>
+            <span>Coming soon...</span>
+          </span>
+        </div>
+        <div className='right-section-button-game'>
+          <button className='Game-button'>?</button>
+        </div>
+      </div>
       <BoosterContainer />
       <FriendsConnt />
       <div className='ButtonGroup'>
