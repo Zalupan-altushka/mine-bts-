2bb9e1d46bf41a0409e69391ab83497e9e3f8e89
Kishibe14 <27.08.07@mail.ru>
2025-04-14 03:44:35 +1100

    36 commit

--- src/Pages/Friends/Friends.jsx
+++ src/Pages/Friends/Friends.jsx
@@ -7,7 +7,7 @@ import Reward from './Containers-fr/Reward/Reward';
 function Friends() {
   const handleInviteClick = () => {
     const message = "Join me in Mine BTS! and let's mine new gold! Use my invite link to join🎉";
-    const url = "https://t.me/MineBtsBot/mine_bts"; // Replace with your link
+    const url = "https://t.me/MineBtsBot/btsapp"; // Replace with your link
     const telegramLink = `tg://msg_url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(message)}`;
     
     window.open(telegramLink, '_blank');
