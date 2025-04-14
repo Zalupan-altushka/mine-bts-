10ac14c100e171b77c50fe51059af3dc09a0d360
Kishibe14 <27.08.07@mail.ru>
2025-04-14 16:16:26 +1100

    48 commit

--- src/Pages/Friends/Friends.jsx
+++ src/Pages/Friends/Friends.jsx
@@ -5,14 +5,7 @@ import Bonus from './Containers-fr/Bonuses/Bonus';
 import Reward from './Containers-fr/Reward/Reward';
 
 function Friends() {
-  const handleInviteClick = () => {
-    const message = "Join me in Mine BTS! and let's mine new gold! Use my invite link to join";
-    const url = "https://t.me/A_D_I_Coin_bot/mine_bts"; // Replace with your link
-    const telegramLink = `tg://msg_url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(message)}`;
-    
-    window.open(telegramLink, '_blank');
-  };
-
+ 
   return (
     <section className='bodyfriendspage'>
       <div className='margin-div-fr'></div>
@@ -21,7 +14,7 @@ function Friends() {
       <Reward />
       <section className='Container-button'>
         <button className='get-reward-button'>Claim Reward</button>
-        <button className='Invite-button' onClick={handleInviteClick}>Invite Friends</button>
+        <button className='Invite-button'>Invite Friends</button>
       </section>
       <Menu />
     </section>
