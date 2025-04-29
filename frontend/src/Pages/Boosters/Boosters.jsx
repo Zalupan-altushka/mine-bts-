fdad8d08745934cf2fc75966ba0d65be448e6754
Kishibe14 <27.08.07@mail.ru>
2025-04-29 16:39:44 +1100

    218

--- frontend/src/Pages/Boosters/Boosters.jsx
+++ frontend/src/Pages/Boosters/Boosters.jsx
@@ -3,31 +3,14 @@ import Menu from '../../Most Used/Menu/Menu';
 import ListContainerThree from '../Boosters-list/ListContainetThree';
 import ListsContainerFirst from '../Boosters-list/ListContainerFirst';
 import ListsContainerSecond from '../Boosters-list/ListContainerSecond';
-import Star from '../../Most Used/Image/Star';
+import BoostersBox from './Containers/BoostersBox';
 
 function Boosters() {
 
   return (
       <section className='bodyboostpage'>
         <div className='margin-div-boost'></div>
-        <section className='boosters-box'>
-          <article className='height-section-box'>
-            <div className='left-section-box'>
-              <span className='first-span'>Boosters Market</span>
-              <span className='two-span'>Please buy boosters with TG Stars</span>
-            </div>
-            <div className='right-section-box'>
-              <Star />
-            </div>
-          </article>
-          <div className='polosa' />
-          <article className='middle-section-box'>
-            <div className='center-section-middle'>
-              <span>Balance: 0.0333</span>
-            </div>
-            <button>Claim</button>
-          </article>
-        </section>
+        <BoostersBox />
         <div className='containers-scroll-wrapper'>
           <div className='center-content'>
             <ListsContainerFirst />
---
+++ frontend/src/Pages/Boosters/Containers/BoostersBox.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Star from '../../../Most Used/Image/Star';
+import './BoostersBox.css'
+
+function BoostersBox() {
+
+  return (
+        <section className='boosters-box'>
+          <article className='height-section-box'>
+            <div className='left-section-box'>
+              <span className='first-span'>Boosters Market</span>
+              <span className='two-span'>Please buy boosters with TG Stars</span>
+            </div>
+            <div className='right-section-box'>
+              <Star />
+            </div>
+          </article>
+          <div className='polosa' />
+          <article className='middle-section-box'>
+            <div className='center-section-middle'>
+              <span>Balance: 0.0333</span>
+            </div>
+            <button>Claim</button>
+          </article>
+        </section>
+  );
+}
+
+export default BoostersBox;
