663a3fbbcb9e4dc118aad35cb0d99c14ed33afff
Kishibe14 <27.08.07@mail.ru>
2025-05-29 16:57:56 +1100

    390

--- frontend/src/Pages/Loader/Loader.jsx
+++ frontend/src/Pages/Loader/Loader.jsx
@@ -1,33 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Loader.css'; // Импортируем стили для анимации
 
-const Loader = ({ success }) => {
-    const [showSuccess, setShowSuccess] = useState(false);
-
-    useEffect(() => {
-        if (success) {
-            setShowSuccess(true); // Показываем "Success!"
-            const timer = setTimeout(() => {
-                setShowSuccess(false); // Скрываем "Success!" через 2 секунды
-            }, 2000);
-
-            return () => clearTimeout(timer); // Очищаем таймер при размонтировании компонента
-        } else {
-            setShowSuccess(false); // Убедитесь, что "Success!" не отображается при неудачной авторизации
-        }
-    }, [success]);
-
+const Loader = () => {
     return (
         <div className="loader">
             <div className="spinner"></div>
-            <div className="text-container">
-                <p className="welcome-text">Welcome!</p>
-                {showSuccess ? (
-                    <p className="status-text-good">Success!</p>
-                ) : (
-                    <p className="status-text">Checking authorization...</p>
-                )}
-            </div>
         </div>
     );
 };
