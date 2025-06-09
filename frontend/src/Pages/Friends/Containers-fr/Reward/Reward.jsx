837606f7bcf7d6d45067370536500413e5853ca6
Kishibe14 <27.08.07@mail.ru>
2025-06-09 23:56:39 +1100

    ref 43

--- frontend/src/Pages/Friends/Containers-fr/Reward/Reward.jsx
+++ frontend/src/Pages/Friends/Containers-fr/Reward/Reward.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import Fly from '../../../../Most Used/Image/Fly';
 import './Reward.css';
 
-function Reward() {
-    const fixedReward = 205.033;
+function Reward({ rewardPoints }) {
 
     return (
         <section className='reward-section-fr'>
             <article className='left-section-reward-fr'>
                 <span className='title-total-frends'>Your total reward!</span>
-                <span className='span-reward-fr'>{fixedReward.toFixed(3)}</span>
+                <span className='span-reward-fr'>{rewardPoints}</span> {/* Используем пропс rewardPoints */}
                 <span className='span-about-fr'>+205.033 BTS for friend</span>
             </article>
             <article className='right-section-reward-fr'>
