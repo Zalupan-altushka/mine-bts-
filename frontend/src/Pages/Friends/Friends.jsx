9900acb194fb38ba366939f0835e7c686af97750
Kishibe14 <27.08.07@mail.ru>
2025-06-12 15:54:08 +1100

    ref 55

--- frontend/src/Pages/Friends/Friends.jsx
+++ frontend/src/Pages/Friends/Friends.jsx
@@ -4,35 +4,46 @@ import Menu from '../../Most Used/Menu/Menu';
 import TotalFR from './Containers-fr/Total/TotalFR';
 import Bonus from './Containers-fr/Bonuses/Bonus';
 import Reward from './Containers-fr/Reward/Reward';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 function Friends({ userData }) {
     const [inviteLink, setInviteLink] = useState('');
 
-    const handleInviteClick = () => {
-        if (!inviteLink) {
-            console.warn("Invite link not found, cannot generate invite link.");
-            return;
-        }
-
-        const message = "Join me in 'Mine BTS!' and let's mine new gold! Use my invite link to join🎉";
-        const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(inviteLink)}&text=${encodeURIComponent(message)}`;
-        window.open(telegramUrl, '_blank');
-    };
-
     useEffect(() => {
-        // Fetch updated user data
-        const fetchUserData = async () => {
-            try {
-                const response = await fetch(`https://ah-user.netlify.app/.netlify/functions/auth?userId=${userData.telegram_user_id}`);
-                const data = await response.json();
-                setInviteLink(data.userData.invite_link);
-            } catch (error) {
-                console.error("Error fetching user data:", error);
+        const fetchInviteLink = async () => {
+            if (userData?.telegram_user_id) {
+                const { data, error } = await supabase
+                    .from('users')
+                    .select('invite_link')
+                    .eq('telegram_user_id', userData.telegram_user_id)
+                    .single();
+
+                if (error) {
+                    console.error("Error fetching invite link:", error);
+                } else if (data) {
+                    setInviteLink(data.invite_link);
+                }
             }
         };
 
-        fetchUserData();
+        fetchInviteLink();
     }, [userData]);
+
+    const handleInviteClick = () => {
+        const userId = userData?.telegram_user_id;
+        if (!userId) {
+            console.warn("User ID not found, cannot generate invite link.");
+            return;
+        }
+
+        const finalInviteLink = inviteLink ;
+
+        const message = "Join me in 'Mine BTS!' and let's mine new gold! Use my invite link to join🎉";
+        const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(finalInviteLink)}&text=${encodeURIComponent(message)}`;
+        window.open(telegramUrl, '_blank');
+    };
 
     return (
         <section className='bodyfriendspage'>
