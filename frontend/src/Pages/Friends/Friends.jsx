43b151d956005bff4d9e2622555f1d896bc654ad
Kishibe14 <27.08.07@mail.ru>
2025-06-12 15:58:56 +1100

    qsadas

--- frontend/src/Pages/Friends/Friends.jsx
+++ frontend/src/Pages/Friends/Friends.jsx
@@ -4,33 +4,8 @@ import Menu from '../../Most Used/Menu/Menu';
 import TotalFR from './Containers-fr/Total/TotalFR';
 import Bonus from './Containers-fr/Bonuses/Bonus';
 import Reward from './Containers-fr/Reward/Reward';
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 function Friends({ userData }) {
-    const [inviteLink, setInviteLink] = useState('');
-
-    useEffect(() => {
-        const fetchInviteLink = async () => {
-            if (userData?.telegram_user_id) {
-                const { data, error } = await supabase
-                    .from('users')
-                    .select('invite_link')
-                    .eq('telegram_user_id', userData.telegram_user_id)
-                    .single();
-
-                if (error) {
-                    console.error("Error fetching invite link:", error);
-                } else if (data) {
-                    setInviteLink(data.invite_link);
-                }
-            }
-        };
-
-        fetchInviteLink();
-    }, [userData]);
-
     const handleInviteClick = () => {
         const userId = userData?.telegram_user_id;
         if (!userId) {
@@ -38,12 +13,12 @@ function Friends({ userData }) {
             return;
         }
 
-        const finalInviteLink = inviteLink ;
-
         const message = "Join me in 'Mine BTS!' and let's mine new gold! Use my invite link to join🎉";
-        const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(finalInviteLink)}&text=${encodeURIComponent(message)}`;
+        const inviteLink = `https://t.me/mine_bts_bot/zZ22?ref=${userId}`;
+        const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(inviteLink)}&text=${encodeURIComponent(message)}`;
         window.open(telegramUrl, '_blank');
     };
+
 
     return (
         <section className='bodyfriendspage'>
