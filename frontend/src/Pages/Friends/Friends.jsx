bc6478680d9889ea803cb7ff670e48ca23b95f48
Kishibe14 <27.08.07@mail.ru>
2025-06-13 17:12:59 +1100

    ref 63

--- frontend/src/Pages/Friends/Friends.jsx
+++ frontend/src/Pages/Friends/Friends.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Friends.css';
 import Menu from '../../Most Used/Menu/Menu';
 import TotalFR from './Containers-fr/Total/TotalFR';
@@ -6,8 +6,6 @@ import Bonus from './Containers-fr/Bonuses/Bonus';
 import Reward from './Containers-fr/Reward/Reward';
 
 function Friends({ userData }) {
-    const [telegramUrl, setTelegramUrl] = useState(''); // Добавляем состояние для URL
-
     const handleInviteClick = () => {
         const inviteLink = userData?.invite_link;
         if (!inviteLink) {
@@ -16,9 +14,9 @@ function Friends({ userData }) {
         }
 
         const message = "Join me in 'Mine BTS!' and let's mine new gold! Use my invite link to join🎉";
-        const url = `https://t.me/share/url?url=${encodeURIComponent(inviteLink)}&text=${encodeURIComponent(message)}`;
-        setTelegramUrl(url); // Обновляем состояние с URL
-        console.log("Friends.jsx: telegramUrl:", url); // Все еще логируем в консоль для отладки
+        const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(inviteLink)}&text=${encodeURIComponent(message)}`;
+        localStorage.setItem('referralCode', userData.telegram_user_id); // Save to localStorage
+        window.open(telegramUrl, '_blank');
     };
 
     return (
@@ -26,12 +24,6 @@ function Friends({ userData }) {
             <div className='margin-div-fr'></div>
             <TotalFR totalFriends={userData?.total_fr} />
             <Bonus />
-            {telegramUrl && ( // Условно отображаем URL, если он есть
-                <div className="telegram-url">
-                    <p>Generated Telegram URL:</p>
-                    <a href={telegramUrl} target="_blank" rel="noopener noreferrer">{telegramUrl}</a>
-                </div>
-            )}
             <Reward />
             <section className='Container-button'>
                 <button className='get-reward-button'>Claim Reward</button>
