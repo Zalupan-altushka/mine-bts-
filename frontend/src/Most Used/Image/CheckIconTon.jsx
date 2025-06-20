e7d35f2a1b29f7fe695dc63b4adc3d5f2a373357
Kishibe14 <27.08.07@mail.ru>
2025-06-21 03:55:53 +1100

    размер айкон

--- frontend/src/Most Used/Image/CheckIconTon.jsx
+++ frontend/src/Most Used/Image/CheckIconTon.jsx
@@ -5,7 +5,7 @@ function CheckIconTon() {
 
   return(
     <div>
-      <img src={checkicon2} style={{marginBottom: "10px"}}alt="Star" />
+      <img src={checkicon2} className='.check-icon-ton' alt="Star" />
     </div>
   );
 }
